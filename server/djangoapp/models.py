67055a12020159d7d8b5df6e7f711398285430e7
FacuB <you@example.com>
2023-03-19 19:36:23 -0400

    models commit

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,7 +24,23 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'sedan'
+    SUV = 'SUV'
+    WAGON = 'WAGON'
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'WAGON')
+    ]
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    dealer_id = models.IntegerField()
+    car_type = models.CharField(max_length=10, choices=CAR_TYPES, default=SEDAN)
+    year = models.DateField(default=now)
 
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
