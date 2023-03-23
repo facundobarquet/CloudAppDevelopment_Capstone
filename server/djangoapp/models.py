e3d1bf160077dd963d8b87399ac49fdfbc461a17
facundobarquet <110636222+facundobarquet@users.noreply.github.com>
2023-03-23 19:07:31 -0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,105 +1,108 @@
-from django.db import models
-from django.utils.timezone import now
+import requests
+import json
+# import related models here
+from .models import CarDealer, DealerReview
+from requests.auth import HTTPBasicAuth
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_watson.natural_language_understanding_v1 \
+    import Features, SentimentOptions
 
 
-# Create your models here.
+# Create a `get_request` to make HTTP GET requests
+# e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
+#                                     auth=HTTPBasicAuth('apikey', api_key))
+def get_request(url, **kwargs):
+    print(kwargs)
+    print("GET from {} ".format(url))
+    try:
+        response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs)
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-class CarMake(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=1000)
+# Create a `post_request` to make HTTP POST requests
+# e.g., response = requests.post(url, params=kwargs, json=payload)
+def post_request(url, json_payload, **kwargs):
+    print(kwargs)
+    print("POST to {} ".format(url))
+    try:
+        response = requests.post(url, params=kwargs, json=json_payload)
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
-    def __str__(self):
-        return self.name
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
-class CarModel(models.Model):
-    SEDAN = 'sedan'
-    SUV = 'SUV'
-    WAGON = 'wagon'
-    COUPE = 'coupe'
-    HATCHBACK = 'hatchback'
-    CONVERTIBLE = 'convertible'
-    CAR_TYPES = [
-        (SEDAN, 'Sedan'),
-        (SUV, 'SUV'),
-        (WAGON, 'Wagon'),
-        (COUPE, 'Coupe'),
-        (HATCHBACK, 'Hatchback'),
-        (CONVERTIBLE, 'Convertible')
-    ]
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    dealer_id = models.IntegerField()
-    car_type = models.CharField(max_length=20, choices=CAR_TYPES, default=SEDAN)
-    year = models.DateField()
+# Create a get_dealers_from_cf method to get dealers from a cloud function
+# def get_dealers_from_cf(url, **kwargs):
+# - Call get_request() with specified arguments
+# - Parse JSON results into a CarDealer object list
+def get_dealers_from_cf(url, **kwargs):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url)
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealers = json_result["result"]
+        # For each dealer object
+        for dealer in dealers:
+            # Get its content in `doc` object
+            dealer_doc = dealer["doc"]
+            # Create a CarDealer object with values in `doc` object
+            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"], state=dealer_doc["state"], 
+                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+    return results
 
-    def __str__(self):
-        return self.name
+# Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
+# def get_dealer_by_id_from_cf(url, dealerId):
+# - Call get_request() with specified arguments
+# - Parse JSON results into a DealerView object list
+def get_dealer_reviews_from_cf(url, dealerId):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url, **{"id":dealerId})
+    if json_result:
+        # Get the row list in JSON as dealers
+        reviews = json_result["body"]
+        # For each dealer object
+        review_data = reviews["data"]
+        for review in review_data:
+            # Get its content in `data` object
+            # Create a DealerReview object with values in `data` object
+            review_obj = DealerReview(dealership=review["dealership"], name=review["name"], purchase=review["purchase"],
+                                   review=review["review"], purchase_date=review["purchase_date"],
+                                   car_make=review["car_make"], car_model=review["car_model"], 
+                                   car_year=review["car_year"], sentiment=analyze_review_sentiments(review["review"]), id=review["id"])
+            results.append(review_obj)
+    return results
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
-class CarDealer:
+# Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
+# def analyze_review_sentiments(text):
+# - Call get_request() with specified arguments
+# - Get the returned sentiment label such as Positive or Negative
+def analyze_review_sentiments(text): 
+    url = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/fb310784-a021-4a1d-90fc-24e3bb0ed300"
+    api_key = "F2JoML5i6pMcpZaqPbGsmOKW9r35-jFcHCyWJe9pi9jA"
+    authenticator = IAMAuthenticator(api_key) 
+    natural_language_understanding = NaturalLanguageUnderstandingV1(version='2022-04-07',authenticator=authenticator) 
+    natural_language_understanding.set_service_url(url)
+    text = text*10
+    response = natural_language_understanding.analyze( text=text ,features=Features(sentiment=SentimentOptions(targets=[text]))).get_result() 
+    label=json.dumps(response, indent=2) 
+    label = response['sentiment']['document']['label'] 
+    return(label) 
 
-    def __init__(self, address, city, state, full_name, id, lat, long, short_name, st, zip):
-        # Dealer id
-        self.id = id
-        # Dealer city
-        self.city = city
-        # Dealer state
-        self.state = state
-        # Dealer st
-        self.st = st
-        # Dealer address
-        self.address = address
-        # Dealer zip
-        self.zip = zip
-        # Location lat
-        self.lat = lat
-        # Location long
-        self.long = long
-        # Dealer short name
-        self.short_name = short_name
-        # Dealer Full Name
-        self.full_name = full_name
 
-    def __str__(self):
-        return self.full_name
 
-# <HINT> Create a plain Python class `DealerReview` to hold review data
-class DealerReview:
-
-    def __init__(self, name, purchase, review, id, dealership, car_model, car_year, sentiment, purchase_date, car_make):
-        # Dealer dealership
-        self.dealership = dealership
-        # Dealer name
-        self.name = name
-        # Dealer purchase
-        self.purchase = purchase
-        # Dealer review
-        self.review = review
-        # Dealer purchase_date
-        self.purchase_date = purchase_date
-        # Dealer car make
-        self.car_make = car_make
-        # Location car model
-        self.car_model = car_model
-        # Location car year
-        self.car_year = car_year
-        # Dealer sentiment
-        self.sentiment = sentiment
-        # Dealer id
-        self.id = id
-
-    def __str__(self):
-        return "Review: " + str(self.review) + " Sentiment: " + str(self.sentiment) + " "
